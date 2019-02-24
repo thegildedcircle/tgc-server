5488eefe257285446557727e6fb86a2f3d973eec
pd-andy <andrew.thompson@qmul.ac.uk>
2019-02-24 15:54:11 +0000

    :recycle: Refactor ECS.Manager into its own file.

--- src/ECS/index.js
+++ src/ECS/index.js
@@ -1,6 +1,7 @@
 import { Entity } from "./entity";
 import { Component } from "./component";
 import { System } from "./system";
+import { Manager } from "./manager";
 
 import { EntityList } from "./utils";
 
@@ -8,49 +9,5 @@ export default {
   Entity,
   Component,
   System,
-  Manager: class Manager {
-    constructor() {
-      this.entityCount = 0;
-      this.entities = new EntityList();
-      this.systems = [];
-
-      return this;
-    }
-
-    addEntity(components = []) {
-      this.entities.push(
-        components.reduce((e, c) => e.addComponent(c), new Entity())
-      );
-
-      return this;
-    }
-
-    removeEntity(id) {
-      this.entities = this.entities.filter(e => e.id !== id);
-
-      return this;
-    }
-
-    getEntities() {
-      return this.entities;
-    }
-
-    updateEntities(predicate, fn) {
-      this.entities = this.entities.filterMap(predicate, fn);
-
-      return this;
-    }
-
-    addSystem(system) {
-      this.systems.push(system);
-
-      return this;
-    }
-
-    runSystems() {
-      this.entities = this.systems.reduce((e, s) => (e = s(e)), this.entities);
-
-      return this;
-    }
-  }
+  Manager
 };
---
+++ src/ECS/manager.js
@@ -0,0 +1,45 @@
+export class Manager {
+  constructor() {
+    this.entityCount = 0;
+    this.entities = new EntityList();
+    this.systems = [];
+
+    return this;
+  }
+
+  addEntity(components = []) {
+    this.entities.push(
+      components.reduce((e, c) => e.addComponent(c), new Entity())
+    );
+
+    return this;
+  }
+
+  removeEntity(id) {
+    this.entities = this.entities.filter(e => e.id !== id);
+
+    return this;
+  }
+
+  getEntities() {
+    return this.entities;
+  }
+
+  updateEntities(predicate, fn) {
+    this.entities = this.entities.filterMap(predicate, fn);
+
+    return this;
+  }
+
+  addSystem(system) {
+    this.systems.push(system);
+
+    return this;
+  }
+
+  runSystems() {
+    this.entities = this.systems.reduce((e, s) => (e = s(e)), this.entities);
+
+    return this;
+  }
+}
